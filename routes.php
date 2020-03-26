df019ae857c0b21fe895ef413840b08d18ca278f
drkuster <60116357+drkuster@users.noreply.github.com>
2020-03-26 16:20:04 -0500

    Deleting extras

--- routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$router->define([
-	"" => "Controllers/index.php",
-	"login" => "Controllers/login.php",
-	"sign-up" => "Controllers/sign-up.php",
-	"dashboard" => "Controllers/dashboard.php"
-]);
